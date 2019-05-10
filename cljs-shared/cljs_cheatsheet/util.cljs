f8f4c12bf80b3ffcdc973b7f4594acb0fe29e64d
Nicolas Barrera <nbarrera@otravo.com>
2019-05-10 15:19:52 +0200

    docs-href conditionally creates link based on cljs namespace or not

--- cljs-shared/cljs_cheatsheet/util.cljs
+++ cljs-shared/cljs_cheatsheet/util.cljs
@@ -22,15 +22,33 @@
 
 (def uri-encode js/encodeURIComponent)
 
-(defn- encode-symbol-url
+(defn- encode-symbol-url-clojuredocs
   "Encode URL for clojuredocs.org"
   [s]
   (-> s
       (replace "/" "_fs") ;; divide symbol - "/"
       (replace "?" "_q")))
 
+(defn- encode-symbol-url-cljs
+  "Encode URL for cljs.github.io"
+  [s]
+  (-> s
+      (replace "/" "SLASH")
+      (replace "?" "QMARK")
+      (replace "!" "BANG")
+      (replace ">" "GT")
+      (replace "<" "LT")
+      (replace "=" "EQ")))
+
 (defn docs-href [name name-space]
-  (str "http://clojuredocs.org/"
-       (uri-encode name-space)
-       "/"
-       (uri-encode (encode-symbol-url name))))
+  (let [cljsns? (= name-space "cljs.core")
+        domain (if cljsns?
+                 "http://cljs.github.io/api/"
+                 "http://clojuredocs.org/")
+        encode-fn (if cljsns?
+                    encode-symbol-url-cljs
+                    encode-symbol-url-clojuredocs)]
+    (str domain
+         (uri-encode name-space)
+         "/"
+         (uri-encode (encode-fn name)))))
